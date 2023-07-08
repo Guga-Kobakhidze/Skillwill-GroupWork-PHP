796d273d9c1da9bd0831f0fdf265e86636efbff5
teotonia <133998254+teotonia@users.noreply.github.com>
2023-07-08 20:55:40 +0400

    Update variables.php

--- Variables/variables.php
+++ Variables/variables.php
@@ -25,17 +25,3 @@ $icons = [
     '<i class="fa fa-facebook" aria-hidden="true"></i>', '<i class="fa fa-twitter" aria-hidden="true"></i>',
     '<i class="fa fa-linkedin" aria-hidden="true"></i>', '<i class="fa fa-instagram" aria-hidden="true"></i>',
 ];
-   $menuItems = [
-        ["name" => "Home", "link" => "index.php"],
-        ["name" => "About", "link" => "about.php"],
-        ["name" => "Treatment", "link" => "treatment.php"],
-        ["name" => "Doctors", "link" => "doctor.php"],
-        ["name" => "Testimonial", "link" => "testimonial.php"],
-        ["name" => "Contact Us", "link" => "contact.php"]
-    ];
-    $socialLinks = [
-        ["icon" => "fa fa-facebook", "link" => "https://www.facebook.com"],
-        ["icon" => "fa fa-twitter", "link" => "https://www.twitter.com"],
-        ["icon" => "fa fa-linkedin", "link" => "https://www.linkedin.com"],
-        ["icon" => "fa fa-instagram", "link" => "https://www.instagram.com"]
-    ];
