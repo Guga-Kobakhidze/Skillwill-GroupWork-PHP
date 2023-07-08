44bf9203aee8304d1528b67c7b2c8adb553e1d6a
G-jvebenava <gjvebenava@gmail.com>
2023-07-08 17:04:54 +0400

    footer-and-testimonials changed

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
+<?php include "./variables/functions.php" ?>
 <?php include_once "./header.php" ?>
 <!-- end header section -->
-
 <!-- about section -->
 <?php include "./indexhtmls.php/about.html" ?>
 <!-- end about section -->
